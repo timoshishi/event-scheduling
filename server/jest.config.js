ff3aeefe8b63eedaa17a5f81d0b807b6df188b49
timoshishi <timfrrst@gmail.com>
2022-10-30 12:57:02 -0400

    chore: move tsconfig and prettier to base

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,9 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  displayName: 'server',
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true,
-  roots: ['./src'],
+  verbose: false,
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['src/**/*.ts*'],
   coveragePathIgnorePatterns: ['src/index.ts', 'src/types/', 'coverage/'],
   coverageThreshold: {
